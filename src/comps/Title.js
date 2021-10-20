88e46ecf7581329060791a2a268590476cfc7b55
Nazmus Sakib <sakib14april@gmail.com>
2021-10-21 01:57:25 +0600

    firebase, firestore deny error fixed

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -5,7 +5,7 @@ const Title = () => {
     <div className="title">
       <h1>NazmusGram</h1>
       <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <p>Please upload your picture to NazmusGram to make beautiful gallery</p>
     </div>
   )
 }
